0c21439aec4318a2c5b7009b4d3e0977001fd272
raspberrysir <141175376+raspberrysir@users.noreply.github.com>
2024-03-28 22:14:18 -0400

    Added basic UI features

--- writers-unblock-application/src/App.js
+++ writers-unblock-application/src/App.js
@@ -1,12 +1,37 @@
 //Sam
 
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
 
+function App() {
+  function Title() {
+    return (
+      
+      <div className="title">
+        Writer's Unblock
+      </div>
+    );
+  }
+
+  function Toggle() {
+
+  }
+
+  function InputField() {
+    return (
+      <div className="inputField">
+        <form className="form">
+          <textarea type="text" className="inputFieldText" placeholder="Enter text here"/>
+        </form>
+      </div>
+    );
+  }
+
+
+  return (
+    <div className="App" >
+      {Title()}
+      {InputField()}
     </div>
   );
 }
