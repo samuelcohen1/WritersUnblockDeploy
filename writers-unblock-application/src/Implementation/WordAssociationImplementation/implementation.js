0ef60788ada42932aa653986ed36329398dd0bad
raspberrysir <141175376+raspberrysir@users.noreply.github.com>
2024-04-07 13:56:01 -0400

    text file reading and comments

--- writers-unblock-application/src/Implementation/WordAssociationImplementation/implementation.js
+++ writers-unblock-application/src/Implementation/WordAssociationImplementation/implementation.js
@@ -3,9 +3,27 @@ const { Map } = require('../DataStructureImplementations/map');
 const { MaxHeap } = require('../DataStructureImplementations/maxheap');
 
 
+const fs = require('fs');
 
 class Implementation {
-    static implement(lastWord) {
+    //reads east of eden text file
+    readFile()  {
+        fs.readFile('../../TextMaterial/EastofEden.txt', (err, data) => {
+            if (err) throw err;
+           
+            return data.toString();
+          });
+    }
+
+    //takes in last word and performs method 1
+    static implement1(lastWord) {
+        let eastOfEden = readFile();
+        return lastWord; // Replace 'bagel' with your actual implementation logic
+    }
+
+    //takes in last word and performs method 2
+    static implement1(lastWord) {
+        let eastOfEden = readFile();
         return lastWord; // Replace 'bagel' with your actual implementation logic
     }
 }
@@ -13,3 +31,10 @@ class Implementation {
 module.exports = Implementation;
 
 //export default Implementation;
+
+async function main() {
+    let implementation = new Implementation();
+    implementation.readFile();
+}
+
+main();
